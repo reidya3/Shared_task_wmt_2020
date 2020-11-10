dcb8fbb0e450167e0e10c7af21b464565df02f9b
scummins00 <seancummins9@gmail.com>
2020-11-10 20:36:48 +0000

    example script to run NER

--- ner/python NER/run_ner.py
+++ ner/python NER/run_ner.py
@@ -1,5 +1,8 @@
 from named_entity_recogniser import *
 
+
+#This is an example of how to use the mask function
+
 IN = "input/train.ende.df.short.tsv"
 OUT = "output/masked.tsv"
 entities = ["PERSON", "ORG", "PRODUCT", "WORK_OF_ART", "LOC"]
