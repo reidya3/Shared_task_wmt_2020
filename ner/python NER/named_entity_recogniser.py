6b04c7a8f4bbbfe9a421840fa8628515f8fd6ff5
scummins00 <seancummins9@gmail.com>
2020-11-10 20:34:31 +0000

    Fixed NER problem. Changed format as requested

--- ner/python NER/named_entity_recogniser.py
+++ ner/python NER/named_entity_recogniser.py
@@ -17,24 +17,24 @@ def mask(input_csv, output_csv, entities):
 	entity_count = 0
 	for index, row in df.iterrows():
 		line = row.original
-		doc=nlp(line)
+		doc = nlp(line)
+		adjust = 0
 		for X in doc.ents:
 			if X.label_ in entities:
+				d[X.text] = "NE{}".format(entity_count)
 
-				d[X.text] = value_creator(X.text, entity_count, index, X.start_char, X.end_char)
+				start, end = X.start_char-adjust, X.end_char-adjust
 
-				before, after = line[:X.start_char], line[X.end_char+1:]
+				line = "".join([line[:start],d[X.text],line[end:]])
 
-				line = " ".join([before, d[X.text][0], after])
-				df.at[index, "original"] = line
+				adjust=len(X.text)-len(d[X.text])
+
 				entity_count+=1
 
+		df.at[index,"original"]=line
+
+			
 	df.to_csv(path_or_buf=output_csv, sep='\t')
 	with open('output/mapping.json', 'w') as fp:
 		json.dump(d, fp)
 	return(d)
-
-def value_creator(text, entity_no, line_no, start, end):
-	length = len(text)
-	val = ((length -(2+len(str(entity_no)))) * "0") + str(entity_no)
-	return("NE{}".format(val),"{}".format(line_no),("{}".format(start), "{}".format(end)))
