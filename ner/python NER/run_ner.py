9401e520ef4555efa43e92803ef8a106a41958c4
scummins00 <seancummins9@gmail.com>
2020-11-26 17:22:26 +0000

    changed to show example reversed ner

--- ner/python NER/run_ner.py
+++ ner/python NER/run_ner.py
@@ -1,10 +1,10 @@
-from named_entity_recogniser2 import mask
+from named_entity_recogniser_reversed import mask
 
 #This is an example of how to use the mask function
 
 IN = "input/dev.roen.df.short.tsv"
-OUT = "output/masked-levenshtein-1/dev.roen.masked.tsv"
-JSON = "output/masked-levenshtein-1/dev.roen.mapping.json"
+OUT = "output/masked-levenshtein-3/dev.roen.masked.tsv"
+JSON = "output/masked-levenshtein-3/dev.roen.mapping.json"
 entities = ["PER", "PERSON", "ORG", "ORGANIZATION", "PRODUCT", "WORK_OF_ART", "LOC"]
 
-mask(IN, OUT, JSON, entities, "roen")
+mask(IN, OUT, JSON, entities)
